a1ed333d447978978b2a764b17778efc86e3e039
suzhongping <suzhongping@phoenixos.com>
2017-01-14 14:12:59 +0800

    add ldap

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ docker build -t namespace/imagename DockerfilePath
 
 run
 ------
-docker run -it --name prefix_name_n [-v ] [-p ] [-e ] -d namespace/imagename
+docker run -it --name prefix_name_n [-v ] [-p ] [-e ] -d namespace/imagename bash
--- openldap/config/openldap.sh
+++ openldap/config/openldap.sh
@@ -3,6 +3,7 @@ set -ex
 
 if [[ -z $SLAPD_ROOT_PASSWORD ]]; then
      echo >&2 "Error: Container not configured and SLAPD_ROOT_PASSWORD not set. "
+     exit 1
 fi
 
 ldif_dir=/etc/ldap/ldif
@@ -13,3 +14,4 @@ ldapmodify -Y EXTERNAL -H ldapi:/// -f $ldif_dir/initRoot.ldif
 ldapmodify -Y EXTERNAL -H ldapi:/// -f $ldif_dir/initAccess.ldif
 ldapadd -f $ldif_dir/addOAndOu.ldif -D cn=admin,dc=mrsu,dc=me -w $SLAPD_ROOT_PASSWORD
 ldapadd -f $ldif_dir/addUser.ldif -D cn=admin,dc=mrsu,dc=me -w $SLAPD_ROOT_PASSWORD
+ldapmodify -Y EXTERNAL -H ldapi:/// -f $ldif_dir/tls.ldif
